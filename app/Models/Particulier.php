ce91260d153025adcce5e0a17aee52d225e2fe17
Karim <karimekg92@gmail.com>
2026-09-03 16:13:03 +0100

    tous les models

--- app/Models/Particulier.php
+++ app/Models/Particulier.php
@@ -10,11 +10,11 @@ class Particulier extends Model
 {
     protected $fillable = ['telephone', 'date_de_naissance'];
 
-    // Un Particulier est lié à UN SEUL Utilisateur (relation 1-1)
+    // Un Particulier est lié à UN SEUL User (relation 1-1)
     // hasOne = "j'ai un seul..."
-    // Permet de faire : $particulier->utilisateur
-    public function utilisateur()
+    // Permet de faire : $particulier->user
+    public function user()
     {
-        return $this->hasOne(Utilisateur::class);
+        return $this->hasOne(User::class);
     }
 }
