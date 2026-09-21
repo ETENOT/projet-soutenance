998c98be3d3aa87918a522930a8f9dc4691c2043
Nicolas <akandakombanicolasaristide@gmail.com>
2026-09-21 11:56:22 +0100

    commentaires explicatifs

--- app/Notifications/VerifyEmailWithCode.php
+++ app/Notifications/VerifyEmailWithCode.php
@@ -6,10 +6,22 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
+/**
+ * Notification envoyée à l'inscription (et à chaque "Renvoyer le code").
+ *
+ * Elle remplace Illuminate\Auth\Notifications\VerifyEmail, la notification
+ * native de Laravel qui envoie un LIEN signé à cliquer. Ici on envoie un CODE
+ * à 6 chiffres à taper, donc on a besoin de notre propre classe : voir
+ * User::sendEmailVerificationNotification() qui l'instancie.
+ */
 class VerifyEmailWithCode extends Notification
 {
     use Queueable;
 
+    /**
+     * $code est injecté au moment de la création de la notification
+     * (cf. User::generateVerificationCode() qui le génère juste avant).
+     */
     public function __construct(protected string $code)
     {
     }
@@ -19,6 +31,12 @@ class VerifyEmailWithCode extends Notification
         return ['mail'];
     }
 
+    /**
+     * Construit et retourne l'email de vérification avec le code
+     *
+     * @param object $notifiable L'entité recevant la notification (ex: User)
+     * @return MailMessage L'email configuré avec le sujet et la vue personnalisée
+     */
     public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
